a2a67da4085ed189e5161538d5d03fae203fa93e
adamkoro <akoronics@protonmail.com>
2022-12-26 12:14:13 +0100

    Terraform variables

---
+++ terraform/libvirt/variables.tf
@@ -0,0 +1,87 @@
+variable "ssh_private_key" {
+  type = string
+}
+
+variable "proxy_vm_count" {
+  default = 2
+  type    = number
+}
+
+variable "proxy_domain_name" {
+  type = string
+}
+
+variable "etcd_vm_count" {
+  default = 3
+  type    = number
+}
+
+variable "etcd_domain_name" {
+  type = string
+}
+
+variable "control-plane_vm_count" {
+  default = 2
+  type    = number
+}
+
+variable "control-plane_domain_name" {
+  type = string
+}
+
+variable "agent_vm_count" {
+  default = 2
+  type    = number
+}
+
+variable "agent_domain_name" {
+  type = string
+}
+
+variable "cloud_init_username" {
+}
+
+variable "cloud_init_password" {
+  type = string
+}
+
+variable "cloud_init_sshkey" {
+  type = string
+}
+
+variable "cloud_init_nameserver" {
+  type = string
+}
+
+variable "cloud_init_search_domain" {
+  type = string
+}
+
+variable "base_root_volume_path" {
+  type = string
+}
+
+variable "root_volume_pool" {
+  type = string
+}
+
+variable "swap_volume_pool" {
+  type = string
+}
+
+variable "etcd_data_volume_pool" {
+  type = string
+}
+
+variable "kubelet_data_volume_pool" {
+  type = string
+}
+
+variable "rancher_data_volume_pool" {
+  type = string
+}
+
+variable "longhorn_data_volume_pool" {
+  type = string
+}
+
