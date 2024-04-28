9f0238ee8836110e57c976b3b4946cc57c4c0654
adamkoro <adam.koronics@gmail.com>
2024-04-28 12:32:48 +0200

    feat: 3 network interfaces

--- terraform/proxmox/downstream/proxy/proxy.tf
+++ terraform/proxmox/downstream/proxy/proxy.tf
@@ -13,7 +13,6 @@ resource "proxmox_vm_qemu" "proxy" {
     bootdisk    = "scsi0"
     startup     = "order=1"
     clone       = var.proxmox_template_name
-    os_type = "cloud-init"
 disks {
         scsi {
             # Root
@@ -39,18 +38,28 @@ disks {
             }
         }
     }
-    # Cloud-init
-    cloudinit_cdrom_storage = var.proxmox_cloudinit_pool
-    cicustom = "user=${var.proxmox_cloudinit_pool}:snippets/cloud_init_${var.proxmox_vm_name}-proxy-${count.index + 1}.yml"
     # Network
     network {
         bridge = "vmbr0"
         model  = "virtio"
     }
-    ipconfig0 = "ip=${var.cloud_init_ip_pool}${count.index + var.cloud_init_ip_increase}/${var.cloud_init_netmask},gw=${var.cloud_init_gateway}"
+    network {
+        bridge = "vmbr1"
+        model  = "virtio"
+    }
+    network {
+        bridge = "adamkoro"
+        model  = "virtio"
+    }
+    os_type = "cloud-init"
+    cloudinit_cdrom_storage = var.proxmox_cloudinit_pool
+    ipconfig0 = "ip=${var.cloud_init_ip_pool0}${count.index + var.cloud_init_ip_increase}/${var.cloud_init_netmask},gw=${var.cloud_init_gateway0}"
+    ipconfig1 = "ip=${var.cloud_init_ip_pool1}${count.index + var.cloud_init_ip_increase}/${var.cloud_init_netmask},gw=${var.cloud_init_gateway1}"
+    ipconfig2 = "ip=${var.cloud_init_ip_pool2}${count.index + var.cloud_init_ip_increase}/${var.cloud_init_netmask}"
+    cicustom = "user=${var.proxmox_cloudinit_pool}:snippets/cloud_init_${var.proxmox_vm_name}-proxy-${count.index + 1}.yml"
     # Check if vm ssh port is open
     provisioner "local-exec" {
-        command = "while ! nc -q0 ${var.cloud_init_ip_pool}${count.index + var.cloud_init_ip_increase} 22 < /dev/null > /dev/null 2>&1; do sleep 10;done"
+        command = "while ! nc -q0 ${var.cloud_init_ip_pool0}${count.index + var.cloud_init_ip_increase} 22 < /dev/null > /dev/null 2>&1; do sleep 10;done"
     }
     # Check if python is installed -- NOT NEEDED
     #provisioner "local-exec" {
@@ -58,6 +67,6 @@ disks {
     #}
     # Run ansible playbook
     provisioner "local-exec" {
-        command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook setup.yaml -i '${var.cloud_init_ip_pool}${count.index + var.cloud_init_ip_increase},' --private-key ${var.ansbile_private_key} -e ansible_user=${var.cloud_init_username}"
+        command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook setup.yaml -i '${var.cloud_init_ip_pool0}${count.index + var.cloud_init_ip_increase},' --private-key ${var.ansbile_private_key} -e ansible_user=${var.cloud_init_username}"
     }
 }
